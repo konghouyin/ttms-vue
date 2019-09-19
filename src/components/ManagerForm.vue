9e4a888b8501045f2f23b03afd53328ad07a784e
zxiyi <1424244330@qq.com>
2019-09-19 17:31:21 +0800

    Manager1.0

---
+++ src/components/ManagerForm.vue
@@ -0,0 +1,74 @@
+<template>
+    <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+      <el-menu >
+        <formfather v-for="(item,index) in formdata" :title="item.title" :index = "index+1+''" :son = "item.son"></formfather>
+      </el-menu>
+    </el-aside>
+</template>
+
+<script>
+   import formfather from './ManagerFormFather.vue'
+  export default {
+    data() {
+          return {
+              formdata:[{title:"剧目管理",
+              son:[{
+                title:"剧目查询",
+                link:"/play/query"
+              },
+              {
+                title:"增加剧目",
+                link:"/play/add"
+                },
+                {
+                title:"修改剧目",
+                link:"/play/change",
+                }
+              ]},
+              {title:"影厅管理",
+              son:[{
+                title:"影厅查询",
+                link:"/cinema/query"
+              },
+              {
+                title:"增加影厅",
+                link:"/cinema/add"
+                },
+                {
+                title:"修改影厅",
+                link:"/cinema/change",
+                }
+              ]},
+              {title:"计划管理",
+              son:[{
+                title:"计划查询",
+                link:"/plan/query"
+              },
+              {
+                title:"增加计划",
+                link:"/plan/add"
+                },
+                {
+                title:"修改计划",
+                link:"/plan/change",
+                }
+              ]}]
+          }
+        },
+    components: {
+      formfather
+    },
+  }
+</script>
+
+<style>
+  .el-header {
+      background-color: #B3C0D1;
+      color: #333;
+      line-height: 60px;
+    }
+
+    .el-aside {
+      color: #333;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,41 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import ManagerTop from '@/components/Managertop'
+import ManagerMain from '@/components/ManagerMain'
+import Hello from '@/components/HelloWorld'
+import manager from '@/components/manager'
+import tableplay from '@/components/ManagerTablePlay'
+import formplay from '@/components/ManagerFormPlay'
+import formtable from '@/components/ManagerBody'
+import mtable from '@/components/ManagerTable'
+import mtable2 from '@/components/ManagerFormPlay'
+import mPlayChange from '@/components/ManagerPlayChange'
+
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes: [{
+    path: '/',
+    component: ManagerMain,
+    children: [{
+        path: '',
+        component: formtable,
+        children: [{
+          path: '',
+          component:mtable
+        },{
+          path: '/play/query',
+          component:mtable
+        },{
+          path: '/play/add',
+          component:mtable2
+        },{
+          path: '/play/change',
+          component:mPlayChange
+        }
+        ]
+      }
+    ]
+  }]
 })
