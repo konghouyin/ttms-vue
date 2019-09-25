eb7d1b1b6e28013b00839f264dba2bf3143f0bf7
leixy <1696380557@qq.com>
2019-09-25 18:57:42 +0800

    lei0.0

---
+++ src/components/MarketRefund.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+		
+		<div class="heh">
+			<el-input v-model="input" placeholder="请输入票号ID" style="width: 250px;"></el-input>
+			<el-button type="primary" icon="el-icon-search">提交</el-button>
+			 <router-link to="/">
+				 <el-button type="primary" plain>返回</el-button>
+			 </router-link>
+		</div>
+		
+	</div>
+</template>
+
+<script>
+	export default{
+		data(){
+			return{
+				input: ''
+			}
+		}
+		
+	}
+</script>
+
+<style scoped>
+	.heh{
+		position: absolute;
+		top: 45%;
+		left: 35%;
+	}
+	
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import MarketMain from '@/components/MarketMain'
+import MarketRefund from '@/components/MarketRefund'
 
 Vue.use(Router)
 
@@ -8,8 +10,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      name: 'MarkentMain',
+      component: MarketMain
+    },
+	{
+		path:'/marketrefund',
+		name:'MarketRefund',
+		component:MarketRefund
+	}
   ]
 })
